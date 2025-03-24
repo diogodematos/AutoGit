999ccbfd1921e7a335cf11a1b5d51d084d1172f1
Dioguinho Matador <mtd5000@gmail.com>
2025-03-24 21:59:33 +0000

    Auto commit 2025-03-24 21:59:33

--- github_script.sh
+++ github_script.sh
@@ -4,15 +4,16 @@ LOG_FILE="$HOME/git_auto_commit.log"
 TODAY=$(date '+%Y-%m-%d')
 
 # Se já foi executado hoje, sai
-if grep -q "$TODAY" "$LOG_FILE"; then
-    echo "Já foi executado hoje. Saindo..."
-    exit 0
-fi
+#if grep -q "$TODAY" "$LOG_FILE"; then
+#    echo "Já foi executado hoje. Saindo..."
+#    exit 0
+#fi
 
 # Executa os comandos Git
+echo "$TODAY" >> "logs.log"
 git add .
 git commit -m "Auto commit $(date '+%Y-%m-%d %H:%M:%S')"
 git push
 
 # Registra a execução
-echo "$TODAY" >> "$LOG_FILE"
+#echo "$TODAY" >> "$LOG_FILE"
